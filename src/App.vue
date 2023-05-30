0dd33f53c18e4059a1c194f6e8527fd22b2746cd
Dasom Ko <ds92ko@gmail.com>
2023-05-31 01:22:00 +0900

    Vue 이벤트 핸들러로 click 감지하기 (허위매물 신고버튼 만들기)까지 수강 완료 && 5분 숙제 완료

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
   <div v-for="(product, idx) in products" :key="idx">
     <h4>{{ product.name }}</h4>
     <p>{{ product.price }}만원</p>
+    <button @click="increase(idx)">허위매물신고</button> <span>신고 횟수: {{ product.reportNum }}</span>
   </div>
 </template>
 
@@ -18,17 +19,25 @@ export default {
       products: [
         {
           name: '역삼동원룸',
-          price: 50
+          price: 50,
+          reportNum: 0
         },
         {
           name: '천호동원룸',
-          price: 60
+          price: 60,
+          reportNum: 0
         },
         {
           name: '마포구원룸',
-          price: 70
+          price: 70,
+          reportNum: 0
         },
       ]
+    }
+  },
+  methods: {
+    increase(idx) {
+      this.products[idx].reportNum++;
     }
   },
   components: {
