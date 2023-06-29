04fccc8f8044899182571c81aa42c5d9d97eafdd
Dasom Ko <ds92ko@gmail.com>
2023-06-30 00:28:25 +0900

    실제 데이터를 박아넣어 상품목록을 만들자 (import / export)까지 수강 완료 && 숙제 완료

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- 모달 -->
   <div v-if="isModalOpen === true" class="modal">
     <div class="modal-content">
       <div class="modal-header">
@@ -13,144 +14,126 @@
       </div>
     </div>
   </div>
+
+  <!-- 네비 -->
   <nav class="menu">
     <a v-for="(navItem, idx) in navList" :key="idx">{{ navItem }}</a>
   </nav>
-  <div v-for="(product, idx) in products" :key="idx">
-    <img class="room-img" :src="product.imgSrc" alt="방 사진">
-    <h4 @click="openModal">{{ product.name }}</h4>
+
+  <!-- 상품 리스트 -->
+  <div v-for="product in products" :key="product.id">
+    <img class="room-img" :src="product.image" alt="room">
+    <h4 @click="openModal">{{ product.title }}</h4>
     <p>{{ product.price }}만원</p>
-    <button @click="increase(idx)">허위매물신고</button> <span>신고 횟수: {{ product.reportNum }}</span>
   </div>
 </template>
 
 <script>
+  import data from './data/products';
 
-export default {
-  name: 'App',
-  data() {
-    return {
-      isModalOpen: false,
-      navList: ['Home', 'Shop', 'About'],
-      products: [
-        {
-          imgSrc: require('./assets/room0.jpg'),
-          name: '역삼동원룸',
-          price: 50,
-          reportNum: 0
-        },
-        {
-          imgSrc: require('./assets/room1.jpg'),
-          name: '천호동원룸',
-          price: 60,
-          reportNum: 0
-        },
-        {
-          imgSrc: require('./assets/room2.jpg'),
-          name: '마포구원룸',
-          price: 70,
-          reportNum: 0
-        },
-      ],
-    }
-  },
-  methods: {
-    increase(idx) {
-      this.products[idx].reportNum++;
+  export default {
+    name: 'App',
+    data() {
+      return {
+        isModalOpen: false,
+        navList: ['Home', 'Shop', 'About'],
+        products: data,
+      }
     },
-    openModal() {
-      this.isModalOpen = true;
+    methods: {
+      openModal() {
+        this.isModalOpen = true;
+      },
+      closeModal() {
+        this.isModalOpen = false;
+      }
     },
-    closeModal() {
-      this.isModalOpen = false;
+    components: {
     }
-  },
-  components: {
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 
-body {
-  margin: 0;
-}
+  body {
+    margin: 0;
+  }
 
-div {
-  box-sizing: border-box;
-}
+  div {
+    box-sizing: border-box;
+  }
 
-.modal {
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, .5);
-  position: fixed;
-  padding: 20px;
-}
+  .modal {
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, .5);
+    position: fixed;
+    padding: 20px;
+  }
 
-.modal-content {
-  width: 100%;
-  background: white;
-  border-radius: 8px;
-  padding: 20px;  
-}
+  .modal-content {
+    width: 100%;
+    background: white;
+    border-radius: 8px;
+    padding: 20px;  
+  }
 
-.modal-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  gap: 10px;
-}
+  .modal-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 10px;
+  }
 
-.modal-header h4 {
-  margin: 0;
-  flex-grow: 1;
-}
+  .modal-header h4 {
+    margin: 0;
+    flex-grow: 1;
+  }
 
-.modal-close-btn {
-  position: relative;
-  background: none;
-  border: 0;
-  padding: 0;
-  width: 30px;
-  height: 30px;
-}
+  .modal-close-btn {
+    position: relative;
+    background: none;
+    border: 0;
+    padding: 0;
+    width: 30px;
+    height: 30px;
+  }
 
-.modal-close-btn .line {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%) rotate(45deg);
-  display: block;
-  width: 100%;
-  height: 2px;
-  background: black;
-}
+  .modal-close-btn .line {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%) rotate(45deg);
+    display: block;
+    width: 100%;
+    height: 2px;
+    background: black;
+  }
 
-.modal-close-btn .line:last-of-type {
-  transform: translate(-50%, -50%) rotate(-45deg);
-}
+  .modal-close-btn .line:last-of-type {
+    transform: translate(-50%, -50%) rotate(-45deg);
+  }
 
-.menu {
-  background: darkslateblue;
-  padding: 15px;
-  border-radius: 5px;
-}
+  .menu {
+    background: darkslateblue;
+    padding: 15px;
+    border-radius: 5px;
+  }
 
-.menu a {
-  color: white;
-  padding: 10px;
-}
+  .menu a {
+    color: white;
+    padding: 10px;
+  }
 
-.room-img {
-  width: 100%;
-  margin-top: 40px;
-}
+  .room-img {
+    width: 100%;
+    margin-top: 40px;
+  }
 </style>
